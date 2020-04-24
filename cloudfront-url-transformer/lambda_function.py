f7dc7e872f064c3aaf3bb9854022bdb058859ef8
Simo Tukiainen <simo@momocode.com>
2020-04-24 15:54:06 +0300

    Redirect /en/ to / and other redirect lambda improvements
    - Redirect /en and /en/ to / (a legacy mapping)
    - Normalize X/index.html to X/ if someone were to request it
    - Improve formatting.

--- cloudfront-url-transformer/lambda_function.py
+++ cloudfront-url-transformer/lambda_function.py
@@ -4,13 +4,15 @@ import re
 FILE_MATCHER = re.compile(".*/[^/]+\\.[^/]+$")
 
 OLD_ARTICLES = [
-'top-10-most-useful-websites-for-everything-english-language',
-'top-5-english-grammar-sites-making-your-feedback-more-actionable',
-'top-5-english-language-game-sites-making-your-feedback-more-actionable',
-'top-5-english-writing-sites-making-your-feedback-more-actionable',
+    "top-10-most-useful-websites-for-everything-english-language",
+    "top-5-english-grammar-sites-making-your-feedback-more-actionable",
+    "top-5-english-language-game-sites-making-your-feedback-more-actionable",
+    "top-5-english-writing-sites-making-your-feedback-more-actionable",
 ]
 
 OLD_URIS = {
+    "/en/": "/",
+    "/en": "/",
     "/user-guide-for-instructors.pdf": "/guides/user-guide-for-instructors.pdf",
     "/user-guide-for-students.pdf": "/guides/user-guide-for-students.pdf",
     "/user-guide-for-team-admins.pdf": "/guides/user-guide-for-team-admins.pdf",
@@ -55,8 +57,13 @@ def redirect_to(new_uri):
 
 def redirect_uri(uri):
 
+    # Normalize paths with trailing slash
     if not is_a_file(uri) and not uri.endswith("/"):
         uri += "/"
+
+    # If someone were to request index.html directly, remove it
+    if uri.endswith("/index.html"):
+        uri = uri[:-10]
 
     return uri
 
